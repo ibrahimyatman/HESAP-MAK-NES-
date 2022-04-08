0a4a165d73e0728b22e747a38bbd3d4c5e969a7f
yatman.ibrahim.73@gmail.com <yatman.ibrahim.73@gmail.com>
2022-04-08 19:31:44 +0300

    HESAP Makinesi örneği

--- Program.cs
+++ Program.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace HESAP_MAKİNESİ
 {
-    public class Program
+    internal class Program
     {
-        public void Main(string[] args)
+        static void Main(string[] args)
         {
             int toplama_sonuc, çıkarma_sonuc,çarpma_sonuc;
             double bölme_sonuc;
@@ -26,9 +26,9 @@ namespace HESAP_MAKİNESİ
                 {
                     if (islem_secimi == "1")
                     {
-                        Console.WriteLine("Lütfen toplamak istediğiniz 1.sayıyı giriniz");
+                        Console.WriteLine("Lütfen toplamak(+) istediğiniz 1.sayıyı giriniz");
                         int sayi1 = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Lütfen toplamak istediğiniz 2.sayıyı giriniz");
+                        Console.WriteLine("Lütfen toplamak(+) istediğiniz 2.sayıyı giriniz");
                         int sayi2 = Convert.ToInt32(Console.ReadLine());
                         toplama_sonuc = sayi1 + sayi2;
                         Console.WriteLine("İşlemin sonucu = " + " " + toplama_sonuc);
@@ -38,39 +38,49 @@ namespace HESAP_MAKİNESİ
                     }
                     else if (islem_secimi == "2")
                     {
-                        Console.WriteLine("Lütfen çıkarmak istediğiniz 1.sayıyı giriniz");
+                        Console.WriteLine("Lütfen çıkarmak (-) istediğiniz 1.sayıyı giriniz");
                         int sayi1 = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Lütfen çıkarmak istediğiniz 2.sayıyı giriniz");
+                        Console.WriteLine("Lütfen çıkarmak (-) istediğiniz 2.sayıyı giriniz");
                         int sayi2 = Convert.ToInt32(Console.ReadLine());
-                        çıkarma_sonuc = sayi1 + sayi2;
+                        çıkarma_sonuc = sayi1 - sayi2;
                         Console.WriteLine("İşlemin sonucu = " + " " + çıkarma_sonuc);
                         break;
                     }
                     else if (islem_secimi=="3")
                     {
-                        Console.WriteLine("Lütfen çarpmak istediğiniz 1.sayıyı giriniz");
+                        Console.WriteLine("Lütfen çarpmak (*) istediğiniz 1.sayıyı giriniz");
                         int sayi1 = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Lütfen çarpmak istediğiniz 2.sayıyı giriniz");
+                        Console.WriteLine("Lütfen çarpmak(*) istediğiniz 2.sayıyı giriniz");
                         int sayi2 = Convert.ToInt32(Console.ReadLine());
-                        çarpma_sonuc = sayi1 + sayi2;
+                        çarpma_sonuc = sayi1 * sayi2;
                         Console.WriteLine("İşlemin sonucu = " + " " + çarpma_sonuc);
                         break;
                     }
                     else if (islem_secimi=="4")
                     {
-                        Console.WriteLine("Lütfen bölmek istediğiniz 1.sayıyı giriniz");
+                        Console.WriteLine("Lütfen bölmek(/) istediğiniz 1.sayıyı giriniz");
                         int sayi1 = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Lütfen bölmek istediğiniz 2.sayıyı giriniz");
-                        int sayi2 = Convert.ToInt32(Console.ReadLine());
-                        bölme_sonuc = sayi1 + sayi2;
+                        Console.WriteLine("Lütfen bölmek(/) istediğiniz 2.sayıyı giriniz");
+                        double sayi2 = Convert.ToDouble(Console.ReadLine());
+                        bölme_sonuc = sayi1 / sayi2;
                         Console.WriteLine("İşlemin sonucu = " + " " + bölme_sonuc);
                         break;
                     }
                     else if (islem_secimi=="q")
                     {
+                        Console.WriteLine("Hesap makinesinden çıkış yapılıyor");
+                        kontrol = false;
+                        break;
 
                     }
+                    else
+                    {
+                        Console.WriteLine("Lütfwn geçerli bir karakter giriniz");
+                        break;
+                           
+                    }
                 }
+                Console.WriteLine("************************************");
 
             }
             
